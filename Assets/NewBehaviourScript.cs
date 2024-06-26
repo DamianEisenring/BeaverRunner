3ef1d71bd51069ae6b1122d04c3dfbeb065cf38d
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-06-26 21:32:14 +0200

    mhm

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 7f;
     public float jumpForce = 10f;
     private Rigidbody2D rb;
     private bool isGrounded;
